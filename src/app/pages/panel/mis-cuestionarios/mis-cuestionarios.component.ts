050dc9947be143142c95074767da33940f2d1c6f
Gabriel <g27r02@gmail.com>
2023-08-29 11:30:00 -0600

    prueba formulario

--- src/app/pages/panel/mis-cuestionarios/mis-cuestionarios.component.ts
+++ src/app/pages/panel/mis-cuestionarios/mis-cuestionarios.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
@@ -13,37 +13,49 @@ export class MisCuestionariosComponent {
   enlaceEdicion: string | null = null;
   enlaceEdicionUsuario: string = '';
 
+  @ViewChild('googleFormFrame', { static: false }) googleFormFrame: ElementRef | undefined;
+
   constructor(
     private sanitizer: DomSanitizer,
     private el: ElementRef,
     private renderer: Renderer2
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
     // Usar el sanitizer para hacer que la URL sea segura
     this.googleFormUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
       `https://docs.google.com/forms/d/e/${this.googleFormId}/viewform?embedded=true`
     );
+
+    // Capturar la URL actual
+    this.enlaceEdicionUsuario = window.location.href;
   }
 
   ngAfterViewInit() {
-    // Espera un breve tiempo para asegurarte de que el DOM se ha renderizado completamente.
-    setTimeout(() => {
-      const iframeElement = this.el.nativeElement.querySelector('#googleForm');
-      if (iframeElement) {
-        // El iframe se ha cargado, ahora puedes buscar el elemento de envío
-        const submitButton = iframeElement.contentDocument.querySelector('.Uc2NEf');
+    const iframeElement = this.googleFormFrame?.nativeElement;
+    if (iframeElement) {
+      iframeElement.addEventListener('load', () => {
+        this.onIframeLoad();
+      });
+    }
+  }
 
-        if (submitButton) {
-          // El botón de envío está en el iframe, puedes realizar acciones aquí
-          console.log('El botón de envío está en el iframe');
-        } else {
-          console.log('El botón de envío no está en el iframe');
-        }
+  onIframeLoad() {
+    const iframe = this.googleFormFrame?.nativeElement;
+    const iframeContent = iframe?.contentWindow?.document;
+
+    // Ahora puedes buscar y manipular elementos dentro del iframe
+    if (iframeContent) {
+      const spanElement = iframeContent.querySelector('span');
+
+      if (spanElement) {
+        // El span se encontró dentro del iframe
+        console.log('El span dentro del iframe está en el DOM');
       } else {
-        console.log('googleFormFrame es undefined');
+        // El span no se encontró dentro del iframe
+        console.log('El span dentro del iframe aún no está en el DOM');
       }
-    });
+    }
   }
 
   handleClick() {
@@ -57,26 +69,10 @@ export class MisCuestionariosComponent {
     } else {
       console.log('no existe');
     }
-
-    this.buscarElementoUc2NEf();
-  }
-
-  buscarElementoUc2NEf() {
-    setTimeout(() => {
-      const divElement = this.el.nativeElement.querySelector('.Uc2NEf');
-      if (divElement) {
-        console.log('El div con la clase Uc2NEf está en el DOM');
-      } else {
-        console.log('El div con la clase Uc2NEf aún no está en el DOM');
-      }
-    });
   }
 
   iniciarEdicion() {
     // Establece el enlace a la página de edición de Google Forms
     this.enlaceEdicion = 'https://docs.google.com/forms/u/0/edit';
   }
-
-
 }
-
