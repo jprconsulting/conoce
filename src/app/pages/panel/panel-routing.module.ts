0c75e262e7e1c73ae4dcc7572dab81a8c4124445
Gabriel <g27r02@gmail.com>
2023-08-30 09:22:27 -0600

    enlace faq usuarios

--- src/app/pages/panel/panel-routing.module.ts
+++ src/app/pages/panel/panel-routing.module.ts
@@ -45,7 +45,11 @@ const routes: Routes = [
         loadChildren: () => import('./mis-cuestionarios/mis-cuestionarios.module')
           .then(m => m.MisCuestionariosModule)
       },
-
+      {
+        path: 'faq/usuarios',
+        loadChildren:() => import("./usuarios/usuarios.module")
+        .then(c => c.UsuariosModule)
+      },
     ]
   }
 ];
