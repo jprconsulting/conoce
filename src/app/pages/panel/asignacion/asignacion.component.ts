0c7c2a1f1ab56758b83358d3f0972db680858ead
ximena-armaas <114454320+ximena-armaas@users.noreply.github.com>
2023-08-31 12:50:02 -0600

    Update componente Asignacion de formularios

--- src/app/pages/panel/asignacion/asignacion.component.ts
+++ src/app/pages/panel/asignacion/asignacion.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
-
-
+import { Component, OnInit } from '@angular/core';
+import { CandidatoService } from 'src/app/core/services/candidato.service';
 @Component({
   selector: 'app-asignacion',
   templateUrl: './asignacion.component.html',
   styleUrls: ['./asignacion.component.css']
 })
-export class AsignacionComponent {
-
+export class AsignacionComponent implements OnInit {
+  candidatos: any[] = []; // Aquí almacenaremos los datos de los candidatos
+  constructor(private candidatoService: CandidatoService) {
+  }
+  ngOnInit() {
+    // Llamar al servicio para obtener los datos de los candidatos
+    this.candidatoService.getCandidatos().subscribe((data: any) => {
+      this.candidatos = data;
+    });
+  }
 }
