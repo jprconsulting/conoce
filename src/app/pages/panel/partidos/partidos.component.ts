86dd0351b036c3d5b38203d42c5dce6029140126
Gabriel <g27r02@gmail.com>
2023-09-06 16:19:20 -0600

    CANDIDATURAS

--- src/app/pages/panel/partidos/partidos.component.ts
+++ src/app/pages/panel/partidos/partidos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-partidos',
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./partidos.component.css']
 })
 export class PartidosComponent {
+  nombrePartido: string = '';
+  previewImage: string | ArrayBuffer | null = null;
 
+  @ViewChild('imagenInput') imagenInput!: ElementRef;
+
+  // Método para abrir el modal
+  openModal() {
+    this.previewImage = null;
+  }
+
+  // Método para cerrar el modal y limpiar los campos
+  closeModal() {
+    this.previewImage = null;
+    this.nombrePartido = '';
+    this.imagenInput.nativeElement.value = '';
+  }
+
+  // Método para manejar el cambio de la imagen
+  onImageChange(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.previewImage = reader.result;
+      };
+    }
+  }
+
+  // Método para guardar el partido político
+  guardarPartido() {
+    this.closeModal();
+  }
+
+    // Método para guardar coalicion
+    guardarCoalicion() {
+      this.closeModal();
+    }
 }
