2d19115ee5abaee8738b226d3d6e70d8542a3d34
Gabriel <g27r02@gmail.com>
2023-11-07 16:58:46 -0600

    get demarcaciones

---
+++ src/app/core/services/comunidad.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HandleErrorService } from './handle-error.service';
+import { Comunidad } from 'src/app/models/comunidad';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComunidadService {
+  private apiUrl = 'https://localhost:7154/api/comunidad';
+
+  constructor(
+    private http: HttpClient,
+    private handleErrorService: HandleErrorService
+  ) { }
+
+  getComunidades(): Observable<Comunidad[]> {
+    return this.http.get<Comunidad[]>(`${this.apiUrl}/obtener_comunidades`).pipe(
+      catchError(this.handleErrorService.handleError)
+    );
+  }
+
+  agregarComunidad(comunidad: Comunidad): Observable<Comunidad> {
+    return this.http.post<Comunidad>(`${this.apiUrl}/agregar_comunidad`, comunidad).pipe(
+      catchError(this.handleErrorService.handleError)
+    );
+  }
+}
---
+++ src/app/models/distritoLocal.ts
@@ -0,0 +1,8 @@
+export interface DistritoLocal {
+  distritoLocalId: number;
+  nombreDistritoLocal: string;
+  acronimo: string;
+  estatus: boolean;
+  extPet: string;
+  estadoId: number;
+}
