6b90607e375e29e4371e3215a190371ec661ca61
Ruby Montes <ruby.montes.rosas@gmail.com>
2023-11-28 16:32:41 -0600

    tabla candidaturas

--- src/app/models/candidaturas.ts
+++ src/app/models/candidaturas.ts
@@ -1,4 +1,11 @@
 export interface Candidaturas {
   tipoCandidaturaId : number;
-  nombreTipoCandidatura : string;
+  candidaturaId: number;
+  nombreTipoCandidatura: string;
+  nombreCandidatura: string;
+  logo: string;
+  base64Logo:string;
+  estatus:string;
+  imagen: string;
+  acronimo: string;
 }
