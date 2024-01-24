39900fe4f7e412048f444615eda3eadb03eaf730
Ruby Montes <ruby.montes.rosas@gmail.com>
2024-01-24 10:31:19 -0600

    editarFormulario

--- src/app/pages/panel/formulario/formulario.component.ts
+++ src/app/pages/panel/formulario/formulario.component.ts
@@ -19,7 +19,7 @@ export class FormularioComponent {
 
   formulario!: Formulario;
   formularioForm!: FormGroup;
-
+  id!: number;
   formularios: Formulario[] = [];
   formulariosFilter: Formulario[] = [];
   isLoading = LoadingStates.neutro;
@@ -55,12 +55,32 @@ export class FormularioComponent {
     );
   }
 
+  
+
+  submit() {
+    this.isModalAdd ? this.agregar() : this.editar();
+  }
   editar() {
+    this.formulario = this.formularioForm.value as Formulario;
+    this.formulario.configGoogleForm = {
+      type: this.jsonConfig.type,
+      projectId: this.jsonConfig.project_id,
+      privateKeyId: this.jsonConfig.private_key_id,
+      privateKey: this.jsonConfig.private_key,
+      clientEmail: this.jsonConfig.client_email,
+      clientId: this.jsonConfig.client_id,
+      authUri: this.jsonConfig.auth_uri,
+      tokenUri: this.jsonConfig.token_uri,
+      authProviderX509CertUrl: this.jsonConfig.auth_provider_x509_cert_url,
+      clientX509CertUrl: this.jsonConfig.client_x509_cert_url,
+      universeDomain: this.jsonConfig.universe_domain
+    } as ConfigGoogleForm;
+
     this.spinnerService.show();
-    this.formularioService.post(this.formulario).subscribe({
+    this.formularioService.put(this.id,this.formulario).subscribe({
       next: () => {
         this.spinnerService.hide();
-        this.mensajeService.mensajeExito('Formulario guardado correctamente');
+        this.mensajeService.mensajeExito('Formulario editado correctamente');
         this.resetForm();
         this.configPaginator.currentPage = 1;
       },
@@ -70,11 +90,6 @@ export class FormularioComponent {
       },
     });
   }
-
-  submit() {
-    this.isModalAdd ? this.agregar() : this.editar();
-  }
-
   creteForm() {
     this.formularioForm = this.formBuilder.group({
       id: [null],
@@ -165,9 +180,10 @@ export class FormularioComponent {
 
 
   setDataModalUpdate(dto: Formulario) {
+    this.id = dto.id;
      this.isModalAdd = false;
      this.formularioForm.patchValue({
-       formularioId: dto.id,
+       id: dto.id,
        nombreFormulario: dto.nombreFormulario,
        googleFormId: dto.googleFormId,
        spreadsheetId: dto.spreadsheetId,
